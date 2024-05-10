3c3fa071c42a4d430bd83c8cfa282de0f146d80b
Lopez Baasch, Julian <julian.lopez@stefanini.com>
2024-05-10 18:38:53 +0200

    removing unused object

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/Header.css';
-import heroImage from '../assets/hero-image.png';
+// import heroImage from '../assets/hero-image.png';
 
 function Header() {
   return (
